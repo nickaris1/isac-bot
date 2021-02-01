ba4fba9aadb755198eba89278161298e7ad17325
sachi <793170+saacki@users.noreply.github.com>
2021-01-31 23:39:36 -0800

    Update loop.sh

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -6,6 +6,6 @@ do
   echo "running daily_exp_snapshots_manual.js"
   node daily_exp_snapshots_manual.js
   echo "successfully ran both scripts. If there were any errors, you'd see them in this console right now."
-  echo "sleeping for 12 hours now"
+  echo "sleeping for 24 hours now"
   sleep 86400 ;
 done
