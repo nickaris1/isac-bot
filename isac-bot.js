4abe5ff5c57cfb6945fa68b021ba5823a5f6df56
sachi <793170+saacki@users.noreply.github.com>
2021-09-02 10:26:54 -0700

    Update isac-bot.js

--- isac-bot.js
+++ isac-bot.js
@@ -1199,9 +1199,9 @@ function printRankedResult(message, results, order, title) {
   }
 
   if( showMore ) {
-    rankingStr += "\nto see full results click ";
+    rankingStr += "\nto see full results ";
     embed.setURL(url);
-    rankingStr += "[_here._]("+url+")";
+    rankingStr += "[**click here.**]("+url+")";
   }
 
   if( manualAgentExist ) {
